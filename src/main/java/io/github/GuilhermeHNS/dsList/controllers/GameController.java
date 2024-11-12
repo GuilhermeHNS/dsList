46bae02d7206865cf6f8bf5016925dd81aff343b
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2024-11-11 21:15:59 -0300

    Criado camada de controller disponibilizando recurso para listar todos os games registrados

---
+++ src/main/java/io/github/GuilhermeHNS/dsList/controllers/GameController.java
@@ -0,0 +1,25 @@
+package io.github.GuilhermeHNS.dsList.controllers;
+
+import io.github.GuilhermeHNS.dsList.dto.GameMinDTO;
+import io.github.GuilhermeHNS.dsList.entities.Game;
+import io.github.GuilhermeHNS.dsList.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/games")
+public class GameController {
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public ResponseEntity findAll() {
+        List<GameMinDTO> gameList = gameService.findAll();
+        return ResponseEntity.ok(gameList);
+    }
+}
