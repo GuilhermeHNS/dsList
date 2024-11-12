22075e1efcadff0af569592fb4c1a608aa794d3d
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2024-11-12 12:30:24 -0300

    Criado endpoint para busca de game pelo id

--- src/main/java/io/github/GuilhermeHNS/dsList/controllers/GameController.java
+++ src/main/java/io/github/GuilhermeHNS/dsList/controllers/GameController.java
@@ -1,11 +1,13 @@
 package io.github.GuilhermeHNS.dsList.controllers;
 
+import io.github.GuilhermeHNS.dsList.dto.GameDTO;
 import io.github.GuilhermeHNS.dsList.dto.GameMinDTO;
 import io.github.GuilhermeHNS.dsList.entities.Game;
 import io.github.GuilhermeHNS.dsList.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,14 @@ public class GameController {
     public ResponseEntity findAll() {
         List<GameMinDTO> gameList = gameService.findAll();
         return ResponseEntity.ok(gameList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity findById(@PathVariable Long id) {
+        GameDTO response = gameService.findById(id);
+        if (response != null) {
+            return ResponseEntity.ok(response);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/io/github/GuilhermeHNS/dsList/services/GameService.java
+++ src/main/java/io/github/GuilhermeHNS/dsList/services/GameService.java
@@ -1,18 +1,22 @@
 package io.github.GuilhermeHNS.dsList.services;
 
+import io.github.GuilhermeHNS.dsList.dto.GameDTO;
 import io.github.GuilhermeHNS.dsList.dto.GameMinDTO;
 import io.github.GuilhermeHNS.dsList.entities.Game;
 import io.github.GuilhermeHNS.dsList.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> gameList = gameRepository.findAll();
         return gameList.stream()
@@ -21,5 +25,14 @@ public class GameService {
                         x.getYear(),
                         x.getImgUrl(),
                         x.getShortDescription())).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Optional<Game> game = gameRepository.findById(id);
+        if(game.isPresent()) {
+            return new GameDTO(game.get());
+        }
+        return null;
     }
 }
