852d092b6de7e0c482946cbf2ad6c9928db62643
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2024-11-18 20:48:38 -0300

    Alterado configuração de cors e alterado perfil para produção

--- src/main/java/io/github/GuilhermeHNS/dsList/configurations/WebConfig.java
+++ src/main/java/io/github/GuilhermeHNS/dsList/configurations/WebConfig.java
@@ -17,8 +17,11 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods().allowedOrigins(corsOrigins);
-            }
+                registry.addMapping("/**")
+                        .allowedOrigins(corsOrigins.split(","))
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);            }
         };
     }
 
