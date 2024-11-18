7b51777aef826cb1048a30f7f7fe210fdd5deb2c
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2024-11-18 16:08:10 -0300

    Documentado GameController

--- src/main/java/io/github/GuilhermeHNS/dsList/controllers/GameController.java
+++ src/main/java/io/github/GuilhermeHNS/dsList/controllers/GameController.java
@@ -2,6 +2,11 @@ package io.github.GuilhermeHNS.dsList.controllers;
 
 import io.github.GuilhermeHNS.dsList.dto.GameMinDTO;
 import io.github.GuilhermeHNS.dsList.services.GameService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,17 +18,34 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/games")
+@Tag(name = "Games", description = "Endpoint para gerenciamento de Games")
 public class GameController {
     @Autowired
     private GameService gameService;
 
     @GetMapping
+    @Operation(summary = "Busca todos os games",
+            description = "Busca todos os games registrados",
+            tags = {"Games"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Games encontrados com sucesso!"),
+                    @ApiResponse(responseCode = "500", description = "Erro interno no servidor!")
+            })
     public ResponseEntity findAll() {
         List<GameMinDTO> gameList = gameService.findAll();
         return ResponseEntity.ok(gameList);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Busca um game por ID",
+            description = "Busca um game pelo seu ID",
+            tags = {"Games"},
+            parameters = @Parameter(name = "id", description = "ID do Game", example = "1", in = ParameterIn.PATH, required = true),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Game encontrado com sucesso!"),
+                    @ApiResponse(responseCode = "404", description = "Game não encontrado!"),
+                    @ApiResponse(responseCode = "500", description = "Erro interno no servidor!")
+            })
     public ResponseEntity findById(@PathVariable Long id) {
         return ResponseEntity.ok(gameService.findById(id));
     }
